2e15c262323d1de75d9948933f6d22de8ee21c42
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2021-11-13 19:28:12 -0800

    Adding first iteration of the userscript.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# treasury-direct-password-enable-user-script
-A userscript to enable user input (and autocomplete for password managers) on the Treasury Direct website (treasurydirect.gov)
+# Treasury Direct Password Enable User Script
+
+**Description:** A userscript to enable user input (and autocomplete for password managers) on the Treasury Direct website (treasurydirect.gov)
+
+<img src="TreasuryDirect.gov_Login_Screen.png" alt="TreasuryDirect.gov Login Screen" width="600px"/>
+
+Want to use a password manager, or type in your password using the keyboard under your fingertips, and not the on-screen one on TreasuryDirect.gov?
+
+This user script is for you.
+
+## Prerequisites
+
+1) You will need a user script manager installed on the browser you wish to use to login to TreasuryDirect.gov
+
+[Tampermonkey](https://www.tampermonkey.net/) (no affiliation) is a common user script manager.
+
+Below are links for the extension for common browsers below:
+- [Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+- [Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+- [Safari](https://apps.apple.com/app/apple-store/id1482490089)
+- [Microsoft Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)
+- [Opera](https://addons.opera.com/en/extensions/details/tampermonkey-beta/)
---
+++ treasury-direct-password-enable.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        TreasuryDirect Password + Autocomplete Enable
+// @namespace   chriscarini.com
+// @description A userscript to enable user input (and autocomplete for password managers) on the Treasury Direct website (treasurydirect.gov).
+// @author      Chris Carini (https://github.com/ChrisCarini)
+// @match       https://www.treasurydirect.gov/RS/PW-Display.do
+// @version     0.0.1
+// @grant       none
+// @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABJklEQVQ4jdWTsUrDUBSGv7ShWrBUF4kOFoKCm4KDUzMIHXwAR4U8gU9QEBzc+wIZHLt1UXRLJwuaKjiIJZAimg6SlAZ6Sws6SEKiCNG6eKbL+fm/e+5/75XKx5dvTFGZacx/ApDDxWZpISEEYozrC1aVQtRrO973gNr+VkKwHA/DtL/061ddDNMmGE0AyJZ2Do5CA8CaUqB28UDj5omcnGV3Y5mz22fqrS5KMU95fZGcnKFlvwKxDNqOh9sXAHR6AZ1eEO3q9gXndy8cnl4zEGP2tleYm5GTgDQVjCYROMzmx7egFGc/pvKHQCzENEZdU1maz2PFjpsKoGsquqYC8OgOOGncR1oCYDkemHY0nusPMUw7oX9+C9L//gvNakX6NaBZrUgA71kJbfLg49+MAAAAAElFTkSuQmCC
+// @license     https://github.com/ChrisCarini/treasury-direct-password-enable-user-script/blob/main/LICENSE
+// @updateURL   https://raw.githubusercontent.com/ChrisCarini/treasury-direct-password-enable-user-script/main/treasury-direct-password-enable.user.js
+// @downloadURL https://raw.githubusercontent.com/ChrisCarini/treasury-direct-password-enable-user-script/main/treasury-direct-password-enable.user.js
+// ==/UserScript==
+
+Array.from(document.getElementsByClassName('pwordinput'))
+    .forEach((elem) => {
+            elem.removeAttribute('readonly');
+        }
+    );
